dc35fcc3c15269ac65be67c90ec0b1cd842f4cf4
iyasmzn <iyasmzn07@gmail.com>
2020-03-23 19:10:44 +0700

    update content thumbnail

--- home/index.php
+++ home/index.php
@@ -20,8 +20,27 @@
 
 			<!-- Content -->
 			<div class="col-sm-10" id="content">
-				<div class="container pt-4 mt-2">
-					<h5>Direkomendasikan</h5>
+				<div class="container">
+					<div class="video-group py-4 mt-2 w-100">
+						<h5 class="pb-2">Direkomendasikan</h5>
+						<div class="row">
+							<div class="col-lg-3">
+								<a href="#" class="video-group-thumbnail pideo-tumbnel-1">
+									<div class="video-group-thumbnail-tag">
+										<div class="video-group-thumbnail-times">10.03</div>
+									</div>
+								</a>
+								<div class="video-group-caption overflow-hidden mt-1">
+									<div class="row px-2 pt-2">
+										<div class="col-sm-3">
+											<a href=""></a>
+										</div>
+										<div class="col-sm-9">asd</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
 				</div>
 			</div>
 
